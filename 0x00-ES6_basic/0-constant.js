3dfc0e0fa31cb73a0ce6594cf0371117917988ac
Henry Arnold <devharnold@gmail.com>
2024-03-05 05:44:38 +0300

    Late Work

---
+++ 0x00-ES6_basic/0-constant.js
@@ -0,0 +1,13 @@
+export function taskFirst() {
+    const task = 'I prefer const where I can.';
+    return task;
+}
+export function getLast() {
+    return ' is okay';
+}
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+
+    return combination;
+}
