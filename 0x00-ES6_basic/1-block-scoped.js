34c3748186e55da5c2b8eeab9c2d3feead36c3a1
Henry Arnold <devharnold@gmail.com>
2024-03-05 06:01:45 +0300

    make update

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrfalse) {
+    const task = false;
+    const task2 = true;
+
+    if(trueOrfalse) {
+        const task = true;
+        const task2 = false;
+    }
+    return [task, task2];
+}
