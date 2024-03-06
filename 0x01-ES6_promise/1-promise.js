3a4d3af607a47ff9d24ffcd3a22cab55ba82a428
Henry Arnold <devharnold@gmail.com>
2024-03-07 02:27:56 +0300

    second promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+function getFullResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({ status: 200, body: 'photo-profile-1' });
+        } else {
+            reject("The fake API is not running currently");
+        }
+    });
+}
