244cf861e36e8a904d55ce702117804f39adbaf7
Henry Arnold <devharnold@gmail.com>
2024-03-07 03:38:57 +0300

    second promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,10 @@
-function getFullResponseFromAPI() {
+export default function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
         if (success) {
-            resolve({ status: 200, body: 'photo-profile-1' });
+            resolve({
+                status: 200,
+                body: 'Success'
+            });
         } else {
             reject(new Error("The fake API is not running currently"));
         }
