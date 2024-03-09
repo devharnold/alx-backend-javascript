c5b99325e7f7d9e5492215bcf4c5dd18a451db34
Henry Arnold <devharnold@gmail.com>
2024-03-09 03:33:10 +0300

    work

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,27 @@
-class Holberton {
-    constructor(name, length, students) {
-        this.name = names;
-        this.length = size;
-        this.students = [];
-    }
+export default class HolbertonCourse {
+	constructor(name, length, students = []) {
+		this.#checkName(name);
+		this.#checkLength(length);
+		this.#checkStudents(students);
+
+		this._name = name;
+		this._length = length;
+		this._students = students;
+	}
+
+	#checkName(name) {
+		if (typeof name !== 'string' || name.trim() === '') {
+			throw new TypeError('name must be a non-empty string');
+		}
+	}
+	#checkLength(length) {
+		if (typeof length !== 'number' || length <= 0) {
+			throw new TypeError('length must be  a positive number');
+		}
+	}
+	#checkStudents(students) {
+		if (!Array.isArray(students) || students.some(student => typeof student !== 'string' || student.trim() === '')) {
+			throw new TypeError('students must be in an array')
+		}
+	}
 }
