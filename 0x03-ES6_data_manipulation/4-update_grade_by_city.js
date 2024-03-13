8917aa4b0c762d87cf25ae2689d0a7c91bc8c3a6
Henry Arnold <devharnold@gmail.com>
2024-03-13 13:00:26 +0300

    student, id, city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,21 @@
+/**
+ * function that returns an array of students for a specific
+ * city with their new grade
+ * should accept a list of students 
+ */
+
+export default function updateStudentGradeByCity() {
+    const defaultGrade = {grade: 'N/A'}
+
+    if(students instanceof Array) {
+        return students.filter((student) => student.location === city)
+        .map((student) => ({
+            id: student.id,
+            firstName: student.firstName,
+            location: student.location,
+            grade: (newGrades.filter((grade) => grade.studentid === student.id)
+            .pop() || defaultGrade).grade,
+        }));
+    }
+    return [];
+}
