e3c8d2ac8e80a7912d5db3b1398812e56aa0fb8a
Henry Arnold <devharnold@gmail.com>
2024-03-13 10:24:43 +0300

    get-students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+// a function that returns an array of objects
+// each object should have 3 attributes
+
+
+
+
+export default function getListStudents() {
+    return [
+        {id: 1, firstName: '', location: 'San Francisco'},
+        {id: 2, firstName: '', location: 'Columbia'},
+        {id: 5, firstName: '', location: 'San Francisco'}
+    ];
+}
