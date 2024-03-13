a2e5ddee136cd23b45764922fa08fa19efdaebc5
Henry Arnold <devharnold@gmail.com>
2024-03-13 12:15:01 +0300

    with location, city

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+/**
+ * function that returns an array of objects
+ * located in a specific city
+ * must use the filter function on the array.
+ * returns:{Array} of objects
+ */
+export default function getStudentsByLocation(location, city) {
+    if (students instanceof Array) {
+        return students.filter((students) => students.location === city);
+    }
+}
