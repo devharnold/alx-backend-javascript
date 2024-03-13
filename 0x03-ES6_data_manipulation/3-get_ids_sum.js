28de8d428dd469648a12edb180466b8fad7b4da0
Henry Arnold <devharnold@gmail.com>
2024-03-13 12:25:55 +0300

    student id

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -3,4 +3,9 @@
  * should accept a list of students from(getListStudents) as param
  * must use the reduce function
  */
-export default function getStudentIdsSum()
+export default function getStudentIdsSum(students) {
+    if (students instanceof Array) {
+        return students.reduce((student) => student.ids);
+    }
+    return [];
+}
