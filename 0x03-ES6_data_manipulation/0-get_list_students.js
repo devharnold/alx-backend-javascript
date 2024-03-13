706d0666668095e7b2d47f42d318015b54567b6d
Henry Arnold <devharnold@gmail.com>
2024-03-13 10:40:54 +0300

    corect-list-students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,14 @@
 // a function that returns an array of objects
 // each object should have 3 attributes
 export default function getListStudents() {
-    return [
+    let students = [
         {id: 1, firstName: '', location: 'San Francisco'},
         {id: 2, firstName: '', location: 'Columbia'},
         {id: 5, firstName: '', location: 'San Francisco'}
     ];
+
+    let listStudents = students.map(function(element) {
+        return `${element.id} ${element.firstName} ${element.location}`;
+    })
+    console.log(listStudents);
 }
---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+// function getListStudentIds -> returns an array of ids
+// takes one argument, array of objects, same format as getListStudents
+// must use the map() function on the array
+
+export default function getListStudentIds {
+    
+}
