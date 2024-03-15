2c826b5f7e5340efc0c23883328bc8bf34cf6b44
Henry Arnold <devharnold@gmail.com>
2024-03-15 16:40:31 +0300

    make commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,6 +6,12 @@
  */
 
 export default function cleanSet(set, startString) {
-    const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
+    const filteredValues = [];
+
+    for (const value of set) {
+        if (value.startsWith(startString)) {
+            filteredValues.push(value.substring(startString.length));
+        }
+    }
     return filteredValues.join('-');
 }
