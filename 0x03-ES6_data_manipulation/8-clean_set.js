c221e1128e25f0689a79b4324f0a15692b405d25
Henry Arnold <devharnold@gmail.com>
2024-03-15 16:55:57 +0300

    value

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,12 +6,18 @@
  */
 
 export default function cleanSet(set, startString) {
-    const filteredValues = [];
+    const newValue = [];
+    if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+        return '';
+    }
+    for (const value of set.values()) {
+        if (typeof value === 'string' && value.startsWith(startString)) {
+            const valueSubStr = value.substring(startString.length);
 
-    for (const value of set) {
-        if (value.startsWith(startString)) {
-            filteredValues.push(value.substring(startString.length));
+            if (valueSubStr && valueSubStr !== value) {
+                newValue.push(valueSubStr);
+            }
         }
     }
-    return filteredValues.join('-');
+    return newValue.join('-');
 }
