0fa99e96d8ddadef18b484ee2ab49fb416647cd1
Henry Arnold <devharnold@gmail.com>
2024-05-01 03:59:33 +0300

    stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+function displayMessage(message) {
+    console.log(message);
+}
+
+function getUserInput() {
+    return new Promise((resolve, reject) => {
+        process.stdin.once('data', (data) => {
+            resolve(data.toString().trim());
+        });
+    });
+}
+
+async function main() {
+    displayMessage("Welcome to Holberton School, what is your name?");
+    const name = await getUserInput();
+    //display the user's name
+    displayMessage(`Your name is: ${name}`);
+    //display the closing message
+    displayMessage("This important software is now closing");
+}
+main();
