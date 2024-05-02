6288e0ebcd0f9c45b9b848e5f5541a1f608eb4e7
Henry Arnold <devharnold@gmail.com>
2024-05-03 02:20:45 +0300

    task 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const {expect} = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+    it('should return 10', () => {
+        const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        
+        const total = sendPaymentRequestToApi(100, 10);
+        expect(total).to.equal(10);
+        expect(stub.calledOnceWithExactly('SUM', 100, 10)).to.be.true;
+        stub.restore();
+    });
+    });
